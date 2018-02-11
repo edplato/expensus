5436eefbf0b53bb00d5f1f01bfe3421cc46b3f1b
atreidesend <jmeek13@gmail.com>
2018-02-11 23:35:41 +0000

    favicon support, logger support, and adding a dune favicon for fun for now

--- lib/server/index.ts
+++ lib/server/index.ts
@@ -2,12 +2,18 @@ import 'dotenv/config'
 
 import Koa from 'koa'
 import Router from 'koa-router'
+import logger from 'koa-logger'
+import favicon from 'koa-favicon'
+import path from 'path'
 
 const app = new Koa()
 const router = new Router()
 
+app.use(logger())
+app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')))
+
 router.get('/*', async ctx => {
-  ctx.body = 'Hello World!'
+  ctx.body = 'Hello world'
 })
 
 app.use(router.routes())
