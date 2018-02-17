41b3ada5d6085b9f0fb0588b3975341b1c2c535b
atreidesend <jmeek13@gmail.com>
2018-02-17 19:51:15 +0000

    setting up store for main app

--- lib/client/index.tsx
+++ lib/client/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './app'
+import configureStore from './state/store'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const store = configureStore()
+
+// adding a dev object to window to support debugging
+// tslint:disable-next-line:no-string-literal
+window['dev'] = { store }
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
